49e4a19fb8f8251e1da5af658b475074d160d83a
astargh <astargh14@gmail.com>
2021-03-18 23:01:31 +0200

    setup

---
+++ src/layouts/MainLayout/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+const MainLayout = () => {
+    return (<div>
+        <Header />
+        <Sidebar />
+    </div>);
+};
+
+export default MainLayout;
